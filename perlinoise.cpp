aa0841ef5d085a2448a6e4450e1ccf736cbad6e7
u-Squared <double2u2@gmail.com>
2024-10-29 15:42:58 +0300

    faster randomization with/and seed

--- perlinoise.cpp
+++ perlinoise.cpp
@@ -15,24 +15,66 @@ typedef struct{
 	float x, y;
 } vector2;
 
-static bool isPrime(unsigned n) {
-    if (n <= 1) return false;
-    if (n <= 3) return true;
-    if (n % 2 == 0 || n % 3 == 0) return false;
-    for (unsigned i = 5; i * i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0) return false;
-    }
-    return true;
-}
+//static unsigned long int perlinRandTable[256] = {
+//1000000033, 1000001453, 1000000799, 1000000019, 1000001111, 1000001919, 1000002103, 1000000847,
+//    1000002211, 1000000363, 1000002069, 1000000609, 1000002437, 1000000241, 1000000289, 1000001637,
+//    1000001237, 1000001193, 1000001481, 1000000333, 3791322487, 4378845491, 5785545827, 9992095787,
+//    8988917363, 6211088437, 4791668137, 7967411383, 3489655667, 6574135511, 1000000841, 1000000907,
+//    1000001013, 1000000867, 1000001597, 1000000759, 1000001217, 1000002157, 1000001699, 1000002097,
+//    1000001831, 1000002053, 1000001311, 1000002077, 1000000583, 1000001851, 1000001619, 1000001987,
+//    1000002349, 1000000769, 1000001723, 1000001087, 1000001741, 1000000317, 1000000283, 1000001811,
+//    1000001613, 1000000781, 1000001537, 1000002153, 1000000261, 1000000381, 1000001341, 1000002341,
+//    1000000123, 1000000087, 1000002357, 1000002381, 1000001021, 1000001153, 1000001293, 1000000973,
+//    1000000531, 1000001441, 1000000173, 1000002027, 1000002417, 1000001837, 1000000277, 1000001777,
+//    1000000467, 1000000567, 1000001521, 1000000661, 1000002399, 1000001559, 1000001403, 1000002511,
+//    1000000647, 1000002179, 1000002011, 1000002093, 1000001847, 1000002173, 1000000951, 1000001451,
+//    1000000923, 1000001997, 1000001381, 1000000427, 1000000181, 1000001171, 1000000579, 1000002129,
+//    1000001129, 1000001117, 1000001361, 1000001147, 1000001633, 1000000773, 1000001677, 1000002217,
+//    1000002243, 1000000897, 1000001839, 8330026847, 1000000523, 1000002021, 1000001049, 1000002481,
+//    1000002117, 1000001531, 1000001511, 1000002143, 1000001079, 1000001759, 1000001033, 1000001733,
+//    1000001969, 1000000297, 1000002393, 1000001201, 1000001223, 1000001907, 1000001579, 1000002073,
+//    1000001483, 1000001639, 1000001091, 1000000251, 1000001849, 1000000623, 1000002497, 1000000379,
+//    1000002161, 1000001761, 1000000163, 1000002003, 1000002337, 1000000509, 1000001949, 1000002191,
+//    1000002107, 1000002221, 1000000013, 1000000473, 1000000619, 1000000669, 1000001319, 1000001351,
+//    1000001567, 1000000487, 3284157443, 1000002371, 1000001139, 1000001373, 1000000849, 1000001259,
+//    1000001471, 1000001667, 1000000703, 1000000683, 1000002133, 1000001029, 1000001877, 1000001681,
+//    1000001267, 1000000037, 1000000453, 1000001163, 1911520717, 1000000447, 1000002441, 1000000957,
+//    1000001109, 1000002197, 1000001043, 1798835459, 1000002059, 1000001229, 1000002491, 1000000961,
+//    1000002111, 1000000727, 1000000463, 1000000627, 1000001411, 1000002249, 1000001943, 1000001873,
+//    1000001489, 1000000301, 1000000411, 1000001093, 1000002039, 1000002131, 1000001891, 1000000351,
+//    2048419325, 1000001857, 1000001881, 1000002247, 1000002029, 1000002293, 1000001819, 1000002431,
+//    1000001123, 1000001343, 1000002489, 1000002209, 1000002273, 1000002141, 1000000253, 1000001513,
+//    1000000047, 1000000719, 1000000881, 1000001609, 1000001823, 1000002447, 1000000651, 1000001793,
+//    1000002159, 1000000313, 1000000129, 1000000169, 1000000553, 1000001549, 1000001379, 1000001039,
+//    1000002493, 1000000877, 1000001169, 1000000461, 1000001269, 1000000059
+//};
+//
+//
+////picks a random value from perlin_randtab[] and returns it 
+//static unsigned int randomPerlinValue(){
+//	int randomIndex = rand() % 256;
+//	int randomValue = perlinRandTable[randomIndex];
+//
+//	return randomValue;
+//}
+//static bool isPrime(unsigned n) {
+//    if (n <= 1) return false;
+//    if (n <= 3) return true;
+//    if (n % 2 == 0 || n % 3 == 0) return false;
+//    for (unsigned i = 5; i * i <= n; i += 6) {
+//        if (n % i == 0 || n % (i + 2) == 0) return false;
+//    }
+//    return true;
+//}
 
 // Function to generate a random prime in a given range
-unsigned generateRandomPrime(unsigned min, unsigned max) {
-    unsigned candidate;
-    do {
-        candidate = min + rand() % (max - min);
-    } while (!isPrime(candidate));
-    return candidate;
-}
+//unsigned generateRandomPrime(unsigned min, unsigned max) {
+//    unsigned candidate;
+//    do {
+//        candidate = min + rand() % (max - min);
+//    } while (!isPrime(candidate));
+//    return candidate;
+//}
 
 
 //func for genarating unqiue filename
@@ -52,23 +94,29 @@ std::string generateUnqiueFilename(const std::string& baseName, const std::strin
 
 
 //TODO: random number Gen for gradients, to differ noise
-vector2 randomGradient(int ix, int iy){
+vector2 randomGradient(int ix, int iy, unsigned int seed){
+
 
 	//no precomputed gradient means this works for any number of grid coords
 	const unsigned w = 8 * sizeof(unsigned);
 	const unsigned s = w / 2;
-	unsigned a = ix, b = iy;
-	//a *= 3284157443;
-	a *= generateRandomPrime(1000000000, 4294967295); // Use a randomly generated prime
+	unsigned a = ix + seed;
+	unsigned b = iy + seed;
 
+	a *= (3284157443 ^ seed);
+	//a *= generateRandomPrime(1000000000, 4294967295); // Use a randomly generated prime
+	//a *= randomPerlinValue();
 
 	b ^= a << s | a >> w - s;
-	//b *= 1911520717;
-	b *= generateRandomPrime(1000000000, 4294967295); 
-
+	b *= (1911520717 ^ seed);
+	//b *= generateRandomPrime(1000000000, 4294967295); 
+	//b *= randomPerlinValue();
+		
 	a ^= b << s | b >> w - s;
-	//a *= 2048419325;
-	a *= generateRandomPrime(1000000000, 4294967295);
+	a *= (2048419325 ^ seed);
+	//a *= generateRandomPrime(1000000000, 4294967295);
+	//a *= randomPerlinValue();
+
 
 	float random = a * (3.14159265 / ~(~0u >> 1)); //in [0, 2*Pi]
 	
@@ -82,9 +130,9 @@ vector2 randomGradient(int ix, int iy){
 
 
 //compute the dot product of the distance and gradient vectors
-float dotGridGradient(int ix, int iy, float x, float y){
+float dotGridGradient(int ix, int iy, float x, float y, unsigned int seed){
 	//get gradient from interger coords
-	vector2 gradient = randomGradient(ix, iy);
+	vector2 gradient = randomGradient(ix, iy, seed);
 	
 	//compute distance vector 
 	float dx = x - (float)ix;
@@ -102,7 +150,7 @@ float interpolate(float a0, float a1, float w){
 
 
 //sample perlin noise at coords x, y
-float perlin(float x, float y){
+float perlin(float x, float y, unsigned int seed){
 	
 	//Determine grid cell corner coords -> done by converting floats (x, y)  to ints
 	//add 1 to get to the other side of the grid box
@@ -117,13 +165,13 @@ float perlin(float x, float y){
 	float sy = y - (float)y0;
 	
 	//cpmpute and interpolate top two corners (of 1 grid box)
-	float n0 = dotGridGradient(x0, y0, x, y);
-	float n1 = dotGridGradient(x1, y0, x ,y);
+	float n0 = dotGridGradient(x0, y0, x, y, seed);
+	float n1 = dotGridGradient(x1, y0, x ,y, seed);
 	float ix0 = interpolate(n0, n1, sx);
 
 	//compute and interpolate bottom two corners
-	n0 = dotGridGradient(x0, y1, x, y);
-	n1 = dotGridGradient(x1, y1, x ,y);
+	n0 = dotGridGradient(x0, y1, x, y, seed);
+	n1 = dotGridGradient(x1, y1, x ,y, seed);
 	float ix1 = interpolate(n0, n1, sx);
 
 	//Final step: interpolate between the two previously interpolated values, now in y
@@ -135,14 +183,24 @@ float perlin(float x, float y){
 }
 
 
-int main() {
+int main(int argc, char *argv[]) {
 
 	const int windowWidth = 1920;
 	const int windowHeight = 1080;
 
 	const int GRID_SIZE = 400;
 
-	srand(static_cast<unsigned>(time(0))); // Seed the random number generator
+
+	unsigned int seed;
+	if (argc > 1) {
+		seed = static_cast<unsigned int>(std::stoul(argv[1])); // Use the seed from command line
+	} else {
+		seed = static_cast<unsigned int>(time(0)); // Default seed based on current time
+	}
+	srand(seed); // Seed the random number generator for randomPerlinValue() if used elsewhere
+
+	std::cout << "Using seed: " << seed << std::endl;
+
 
 	//create array to store RGB (Maily Alpha) values, 4 values per pixel
 	std::vector<unsigned char> pixels(windowWidth * windowHeight * 4, 0);
@@ -161,7 +219,7 @@ int main() {
 			//loop for the number of octives we want, in this case 12
 			//for each loop cacling the freqs and amps 
 			for (int i = 0; i < 12; i++){
-				val += perlin(x * freq / GRID_SIZE, y * freq / GRID_SIZE) * amp;
+				val += perlin(x * freq / GRID_SIZE, y * freq / GRID_SIZE, seed) * amp;
 				
 				//for gradient/convulution
 				freq *= 2;
